97a8fa1224318557876ab0facd3bfe994f34415a
safoo-1995 <143965356+safoo-1995@users.noreply.github.com>
2024-06-03 15:05:11 +0300

    second commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,74 @@
 import React, { useRef, useState } from 'react'
 
 export default function Navbar() {
-  const [todos, setTodos] = useState([]);
+  const [taskList, setTask] = useState([]);
+  const [indexvalue , setindexValue] = useState(-1);
   const inputRef = useRef();
+
   const addToddos = () => {
-    const text = inputRef.current.value;
-    setTodos([...todos, text]);
-    inputRef.current.value = "";
+   let taskName = document.getElementById('taskName');
+   let btn = document.getElementById('editBtn');
+   if(taskName.value){
+    if(btn.innerText == 'Add'){
+      let deepCopyOfList=[...taskList]
+      deepCopyOfList.push(taskName.value)
+      setTask(deepCopyOfList);
+      
+    }
+    else{
+      let deepCopyOfList=[...taskList] 
+      deepCopyOfList.splice(indexvalue,1,taskName.value)
+      setTask(deepCopyOfList);
+      btn.innerText ='Add';
+
+    }
+    
+   }
+    // setTodos([...todos, text]);
+    // inputRef.current.value = "";
+
   }
+  const DeleteToddos = (id) => {
+    let deepCopyOfList=[...taskList]
+    deepCopyOfList.splice(id,1)
+    setTask(deepCopyOfList);
+}
+const editToddos = (id)=>{
+  let taskName = document.getElementById('taskName');
+  let task = taskList[id];
+  taskName.value = task;
+  document.getElementById('editBtn').innerText='Edit';
+  setindexValue(id);
+}
   return (
     <div>
      <div className =' docontainer'>
      <h1>To Do List</h1>
-     <input ref={inputRef} placeholder='Enter item ...' />
-      <button onClick={addToddos} className='btn btn-primary mx-2' >add</button>
+     <input  id='taskName' ref={inputRef} placeholder='Enter item ...' />
+      <button onClick={addToddos} id='editBtn' className='btn btn-primary mx-2' >Add</button>
 
 
       <table className='border'>
         <thead>
           <tr>
+          <th>#</th>
             <th>tasks</th>
            
           </tr>
         </thead>
-        <tbody>
-        <ul className='color'>
-        {todos.map((item => {
-          return (<li key={indexedDB}>{item}</li>);
-        }))}
-      </ul>
+      
+      <tbody>
+     
+  {taskList.map((task,index)=>( <tr>
+      <td >{index}</td>
+      <td>{task}</td>
+     
+<td>
+<button  onClick={() => DeleteToddos(index)} className='btn btn-danger mx-2'>Delete</button>
+<button onClick={() => editToddos(index)} className='btn btn-warning ' >Edit</button>
+
+</td>
+</tr>))}
         </tbody>
       </table>
      
